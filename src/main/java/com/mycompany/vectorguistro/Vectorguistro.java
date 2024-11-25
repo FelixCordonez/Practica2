bcbee2bf9413d124d7e1f7e3b2b07f78c12ca4bc
Felix Cordonez <felix.08benaicazar@gamail.com>
2024-11-25 16:30:33 -0500

    Proyectosss

--- src/main/java/com/mycompany/vectorguistro/Vectorguistro.java
+++ src/main/java/com/mycompany/vectorguistro/Vectorguistro.java
@@ -21,6 +21,7 @@ public class Vectorguistro {
        obcarros [0] = nuevocarro;
         for (Productos ncar : obcarros) {
             System.out.println(" nuevo: "+ncar);
+            System.out.println(" hola ");
         }
     }
 }
